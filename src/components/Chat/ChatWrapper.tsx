c00a9acf7e40e5271f553322bf92de74952f0d9a
revv2703 <asijaprakash27@gmail.com>
2023-11-29 12:26:49 +0530

    implemnted frontend for chat section

--- src/components/Chat/ChatWrapper.tsx
+++ src/components/Chat/ChatWrapper.tsx
@@ -3,7 +3,9 @@
 import { trpc } from "@/app/_trpc/client"
 import ChatInput from "./ChatInput"
 import Message from "./Message"
-import { Loader2 } from "lucide-react"
+import { ChevronLeft, Loader2, XCircle } from "lucide-react"
+import Link from "next/link"
+import { buttonVariants } from "../ui/button"
 
 interface ChatWrapperProps {
   fileId: string
@@ -30,13 +32,44 @@ const ChatWrapper = ({fileId}: ChatWrapperProps) => {
       <ChatInput />
     </div>
   )
+  if(data?.status === 'PROCESSING') return ( 
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1 flex justify-center items-center flex-col mb-28">
+        <div className="flex flex-col items-center gap-2 ">
+          <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
+          <h3 className="font-semibold text-xl">Processing PDF...</h3>
+          <p className="text-zinc-500 text-sm">This won&apos;t take long.</p>
+        </div>
+      </div>
 
+      <ChatInput />
+    </div>
+  )
+
+  if(data?.status === 'FAILED') return (
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1 flex justify-center items-center flex-col mb-28">
+        <div className="flex flex-col items-center gap-2 ">
+          <XCircle className="h-8 w-8 text-red-500 animate-spin" />
+          <h3 className="font-semibold text-xl">Too many pages in PDF</h3>
+          <p className="text-zinc-500 text-sm">Your <span className="font-medium">Free</span>{' '} plan supports upto 6 pages per PDF.</p>
+          <Link href='/dashboard' className={buttonVariants({
+            variant: "secondary",
+            className: "mt-4"
+          })}><ChevronLeft className="h-3 w-3 mr-1.5" />Back</Link>
+        </div>
+      </div>
+
+      <ChatInput />
+    </div>
+  )
+// 6:20:00
   return (
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
         <div className="flex-1 justify-between flex flex-col mb-28">
           <Message />
         </div>
-        <ChatInput />
+        <ChatInput isDisabled />
     </div>
   )
 }
