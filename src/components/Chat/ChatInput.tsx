9dcef216c2324aea905bf56342c1735b81e6475a
revv2703 <asijaprakash27@gmail.com>
2023-12-06 18:52:48 +0530

    implemented infinite queries and display messages

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -1,7 +1,7 @@
 import { Send } from "lucide-react"
 import { Button } from "../ui/button"
 import { Textarea } from "../ui/textarea"
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { ChatContext } from "./ChatContext"
 
 interface ChatInputProps{
@@ -10,21 +10,34 @@ interface ChatInputProps{
 
 const ChatInput = ({isDisabled} : ChatInputProps) => {
 
-    const {} = useContext(ChatContext)
+    const {addMessage, handleInputChange, isLoading, message} = useContext(ChatContext)
+    const textAreaRef = useRef<HTMLTextAreaElement>(null)
 
     return (
         <div className="absolute bottom-0 left-0 w-full">
-            <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+            <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
                 <div className="relative flex h-full flex-1 items-stretch md:flex-col">
                     <div className="relative flex flex-col w-full flex-grow p-4">
                         <div className="relative">
-                            <Textarea rows={1} maxRows={4} autoFocus placeholder="Enter your question..." className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch" />
+                            <Textarea rows={1} ref={textAreaRef} maxRows={4} autoFocus onChange={handleInputChange} value={message} onKeyDown={(e) => {
+                                if(e.key === "Enter" && !e.shiftKey){
+                                    e.preventDefault()
 
-                            <Button className="absolute bottom-1.5 right-[8px]" aria-label="Send Message"><Send className="h-4 w-4" /></Button>
+                                    addMessage()
+
+                                    textAreaRef.current?.focus
+                                }
+                            }} placeholder="Enter your question..." className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch" />
+
+                            <Button disabled={isLoading || isDisabled} className="absolute bottom-1.5 right-[8px]" aria-label="Send Message" onClick={() => {
+                                addMessage()
+
+                                textAreaRef.current?.focus()
+                            }}><Send className="h-4 w-4" /></Button>
                         </div>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
