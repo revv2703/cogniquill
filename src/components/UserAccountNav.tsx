8a1cd9017e79fb40fc430a508819e7bf6bc5ab65
revv2703 <asijaprakash27@gmail.com>
2023-12-25 12:32:53 +0530

    improved navbar and fixed post login redirect issue

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,9 +1,12 @@
 import { getUserSubscriptionPlan } from "@/lib/stripe"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
 import { Button } from "./ui/button"
 import { Avatar, AvatarFallback } from "./ui/avatar"
 import Image from "next/image"
 import { Icons } from "./Icons"
+import Link from "next/link"
+import { Gem } from "lucide-react"
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components"
 
 interface UserAccountNavProps{
     email: string | undefined
@@ -30,8 +33,38 @@ const UserAccountNav = async ({email, imageUrl, name}: UserAccountNavProps) => {
                     </Avatar>
                 </Button>
             </DropdownMenuTrigger>
-            {/* 10:14:00 */}
-            <DropdownMenuContent></DropdownMenuContent>
+            <DropdownMenuContent className="bg-white" align="end">
+                <div className="flex items-center justify-start gap-2 p-2">
+                    <div className="flex flex-col space-y-0.5 leading-none">
+                        {name && <p className="font-medium text-sm text-black">{name}</p>}
+                        {email && (
+                            <p className="w-[200px] truncate text-sm text-zinc-700">{email}</p>
+                        )}
+                    </div>
+                </div>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem asChild>
+                    <Link href='/dashboard'>Dashboard</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem asChild>
+                    {(await subscriptionPlan).isSubscribed ? (
+                        <Link href='/dashboard/billing'>Manage Subscription</Link>
+                    ): (
+                        <Link href='/pricing'>Upgrade{' '} <Gem className="text-blue-600 h-4 w-4 ml-1.5" /></Link>
+                    )}
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem className="cursor-pointer">
+                    <LogoutLink>Log out</LogoutLink>
+                </DropdownMenuItem>
+                
+            </DropdownMenuContent>
+            
         </DropdownMenu>
     )
 }
